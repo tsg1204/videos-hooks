67b2cbbcfcf86e63b28c277ec7231d60a22fa9c1
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-01 11:19:28 -0500

    refactor App to func comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import youtube from '../apis/youtube';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
+const App = () => {
+    const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
 
-class App extends React.Component {
-    state = {
-        videos: [],
-        selectedVideo: null
-    };
+    useEffect(() => {
+        handleSubmit('buildings');
+    }, []);
 
-    componentDidMount() {
-        this.handleSubmit('buildings');
-    }
-
-    handleSubmit = async term => {
+    const handleSubmit = async term => {
         const response = await youtube.get("/search", {
           params: {
             q: term,
           }
         });
 
-        this.setState({
-            videos: response.data.items,
-            selectedVideo: response.data.items[0]
-        });
-        
+        setVideos(response.data.items);
+        setSelectedVideo(response.data.items[0]);   
     };
 
-    handleVideoSelect = (video) => {
+    const handleVideoSelect = (video) => {
         //console.log(video);
-        this.setState({ selectedVideo: video });
+        setSelectedVideo(video);
     }
 
-    render() {
-        return (
+    return (
         <div className="ui container">
-            <SearchBar onSearchSubmit={this.handleSubmit} />
+            <SearchBar onSearchSubmit={handleSubmit} />
             <div className="ui grid">
                 <div className="ui row">
                     <div className="eleven wide column">
-                        <VideoDetail video={this.state.selectedVideo} />
+                        <VideoDetail video={selectedVideo} />
                     </div>                   
                     <div className="five wide column">
-                        <VideoList videos={this.state.videos} handleVideoSelect={this.handleVideoSelect} />
+                        <VideoList videos={videos} handleVideoSelect={handleVideoSelect} />
                     </div>
 
                 </div>
@@ -52,7 +45,6 @@ class App extends React.Component {
 
         </div>
         );
-    }
 }
 
 export default App;
